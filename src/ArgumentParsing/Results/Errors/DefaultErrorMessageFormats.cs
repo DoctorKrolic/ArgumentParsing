cdf66d844ce78f8516865890995f5538ed0a1a6d
DoctorKrolic <mapmyp03@gmail.com>
2024-04-13 14:11:40 +0300

    Remove quotes around parameter indices in default error messages

--- src/ArgumentParsing/Results/Errors/DefaultErrorMessageFormats.cs
+++ src/ArgumentParsing/Results/Errors/DefaultErrorMessageFormats.cs
@@ -10,7 +10,7 @@ internal static class DefaultErrorMessageFormats
     public const string MissingRequiredOptionError_BothOptionNames = "Missing required option '{0}' ('{1}')";
     public const string BadOptionValueFormatError = "Value '{0}' is in incorrect format for option '{1}'";
     public const string FlagOptionValueError = "Flag option '{0}' does not accept a value";
-    public const string BadParameterValueFormatError = "Value '{0}' is in incorrect format for parameter '{1}' (parameter index '{2}')";
-    public const string MissingRequiredParameterError = "Missing required parameter '{0}' (parameter index '{1}')";
-    public const string BadRemainingParameterValueFormatError = "Value '{0}' is in incorrect format for parameter at index '{1}'";
+    public const string BadParameterValueFormatError = "Value '{0}' is in incorrect format for parameter '{1}' (parameter index {2})";
+    public const string MissingRequiredParameterError = "Missing required parameter '{0}' (parameter index {1})";
+    public const string BadRemainingParameterValueFormatError = "Value '{0}' is in incorrect format for parameter at index {1}";
 }
